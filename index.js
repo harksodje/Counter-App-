342e86055a811a06ca466e5490673a6cda03eecc
harksodje <harksodje@gmail.com>
2022-01-27 21:34:41 +0100

    calculator

--- index.js
+++ index.js
@@ -14,3 +14,39 @@ const save = () =>{
     saveEL.textContent += countStr;
     count = 0
 }
+/*
+let score =  0
+function add(){
+    score += 3;
+    return score;
+}
+
+function sub(){
+    score -=1;
+    return score;
+}
+
+
+let user = add()
+user = sub()
+console.log(user)
+*/
+let num_1 = 12
+let num_2 = 3
+let result = document.getElementById("rsl-el")
+const add = ()=>{
+        result.textContent = 0
+        result.textContent += num_1 + num_2;
+}
+const sub = ()=>{
+    result.textContent = 0
+    result.textContent = num_1 - num_2;
+}
+const div= ()=>{
+    result.textContent = 0
+    result.textContent = num_1 / num_2;
+}
+const mul = ()=>{
+    result.textContent = 0
+    result.textContent = num_1  * num_2;
+}
